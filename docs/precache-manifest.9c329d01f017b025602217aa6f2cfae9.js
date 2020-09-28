a40f173159a08312e77293bc717f1e91c2a0b893
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-28 21:34:42 +0200

    update readme and run docs

---
+++ docs/precache-manifest.9c329d01f017b025602217aa6f2cfae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0584aff5957bea9eaa1dd383295314b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d8073eeb8b193bf1e3d",
+    "url": "./static/css/main.969bd484.chunk.css"
+  },
+  {
+    "revision": "5a75de1ab2fbbb90b3df",
+    "url": "./static/js/2.e146c33b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e146c33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8073eeb8b193bf1e3d",
+    "url": "./static/js/main.5f4c59b5.chunk.js"
+  },
+  {
+    "revision": "b98b6c2c36ee6447dd6f",
+    "url": "./static/js/runtime-main.6b7ebbb5.js"
+  }
+]);
